f41bdd492886d1ca39db37c1d4339d3a22ec07ab
lr159151 <1909265384@qq.com>
2023-01-24 00:49:04 +0800

    feat: the display of the covering layer

--- src/views/entire/c-cpns/entire-rooms/index.tsx
+++ src/views/entire/c-cpns/entire-rooms/index.tsx
@@ -1,7 +1,7 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { RoomsWrapper } from '@/views/entire/c-cpns/entire-rooms/style'
-import { useAppSelector } from '@/store'
+import { shallowEqualApp, useAppSelector } from '@/store'
 import RoomItem from '@/components/room-item'
 
 interface IProps {
@@ -9,18 +9,23 @@ interface IProps {
 }
 
 const EntireRooms: FC<IProps> = () => {
-  const { roomList, totalCount } = useAppSelector((state) => ({
-    roomList: state.entire.roomList,
-    totalCount: state.entire.totalCount
-  }))
+  const { roomList, totalCount, isLoading } = useAppSelector(
+    (state) => ({
+      roomList: state.entire.roomList,
+      totalCount: state.entire.totalCount,
+      isLoading: state.entire.isLoading
+    }),
+    shallowEqualApp
+  )
   return (
     <RoomsWrapper>
       <h2 className="title">{totalCount}多处住宿</h2>
       <div className="list">
         {roomList!.map((item) => (
-          <RoomItem itemData={item} itemWidth="20%" key={item.id} />
+          <RoomItem itemData={item} itemWidth="20%" key={item._id} />
         ))}
       </div>
+      {isLoading && <div className="cover"></div>}
     </RoomsWrapper>
   )
 }
